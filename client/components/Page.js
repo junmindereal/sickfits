d27f1ffdc26a05be2614f116e39e810c9f240151
Junmin De Real <junmindereal@gmail.com>
2020-08-06 12:03:24 +0800

    Added Meta component

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,8 +1,10 @@
 import Header from "./Header";
+import Meta from "./Meta";
 
 const Page = ({ children }) => {
   return (
     <div className="page">
+      <Meta />
       <Header />
       {children}
     </div>
